071bde7fdb9afd16691eb1b82266703210739c65
hyun6ik <fkanwm@naver.com>
2021-02-17 21:34:14 +0900

    입출력

---
+++ 코딩테스트를 위한 파이썬 문법/입출력/입출력.py
@@ -0,0 +1,17 @@
+# n = int(input())
+# data = list(map(int, input().split()))
+#
+# data.sort(reverse=True)
+# print(data)
+
+# n,m,k = map(int, input().split())
+# print(n,m,k)
+
+# import sys
+# data = sys.stdin.readline().rstrip()
+# print(data)
+
+answer = 7
+print("정답은 " + str(answer) + " 입니다.")
+print("정답은", answer, "입니다")
+print(f"정답은 {answer} 입니다")
